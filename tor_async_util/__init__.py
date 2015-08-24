38c26c3605191aaf30c363105429ce5161d99bc9
Dave Simons <simonsdave@gmail.com>
2015-08-24 09:23:27 -0400

    adding more unit tests

--- tor_async_util/__init__.py
+++ tor_async_util/__init__.py
@@ -6,19 +6,33 @@ except ImportError:
     pass
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 _logger = logging.getLogger("tor_async_util.%s" % __name__)
 
 
 def is_libcurl_compiled_with_async_dns_resolver():
-    """If you've configured Tornado to use the async curl_httpclient,
-    per this (http://tornado.readthedocs.org/en/latest/httpclient.html)
-    warning, you'll want to make sure that libcurl has been compiled
-    with async DNS resolver. This function wraps up all the gory details
-    for answering this question and returns True if libcurl has been
-    compiled with async DNS resolver otherwise returns False.
+    """Per this (http://tornado.readthedocs.org/en/latest/httpclient.html),
+    if you've configured Tornado to use async curl_httpclient, you'll want
+    to make sure that libcurl has been compiled with async DNS resolver.
+    The programmatic approach to checking for libcurl being compiled
+    with async DNS resolve is a mess of gory details. It was this mess
+    that drove the need for function. Specifically, this function implements
+    all the gory details so the caller doesn't have to worry about them!
+    If you really want to understand the details start with the following
+    article:
+
+        http://stackoverflow.com/questions/25998063/how-can-i-tell-if-the-libcurl-installed-has-asynchronous-dns-enabled
+
+    Other references that you'll find useful on your question for understanding:
+
+        http://curl.haxx.se/libcurl/
+        https://github.com/bagder/curl/blob/master/include/curl/curl.h#L2286
+
+    If you don't care about the implementation details just know that
+    this function returns True if libcurl has been compiled with async DNS
+    resolver otherwise this functions returns False.
     """
     try:
         version_info = pycurl.version_info()
@@ -32,5 +46,6 @@ def is_libcurl_compiled_with_async_dns_resolver():
             "Error trying to figure out if libcurl is complied with "
             "async DNS resolver - %s"
         )
-        _logger.debug(fmt, ex)
+        msg = fmt % ex
+        _logger.debug(msg)
         return False
---
+++ tor_async_util/tests/unit_tests.py
@@ -0,0 +1,95 @@
+"""This module contains unit testings for __init__.py."""
+
+import unittest
+import sys
+
+import mock
+
+import tor_async_util
+
+
+class MockPycurlInstaller(object):
+
+    def __init__(self, ut, version_info_return_value):
+        object.__init__(self)
+
+        pycurl_mock = mock.Mock()
+        pycurl_mock.version_info.return_value = version_info_return_value
+        self._pycurl_patch = mock.patch(
+            __name__ + ".tor_async_util.pycurl",
+            pycurl_mock,
+            create=True)    # :TRICKY: 'create' important since pycurl doesn't exist
+
+    def __enter__(self):
+        self._pycurl_patch.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._pycurl_patch.stop()
+
+
+class TestCase(unittest.TestCase):
+    """Abstract base class for all TestCase's in this file."""
+
+    def setUp(self):
+        self.assertTrue("pycurl" not in sys.modules)
+        with self.assertRaises(ImportError):
+            import pycurl
+
+    def test_pycurl_import_not_available(self):
+        with mock.patch(__name__ + ".tor_async_util._logger") as logger_patch:
+            self.assertFalse(tor_async_util.is_libcurl_compiled_with_async_dns_resolver())
+
+            self.assertEqual(
+                logger_patch.error.call_args_list,
+                [])
+
+            self.assertEqual(
+                logger_patch.info.call_args_list,
+                [])
+
+            expected_debug_message = (
+                "Error trying to figure out if libcurl is complied with "
+                "async DNS resolver - global name 'pycurl' is not defined"
+            )
+            self.assertEqual(
+                logger_patch.debug.call_args_list,
+                [mock.call(expected_debug_message)])
+
+    def test_happy_version_info_array_does_not_contain_features(self):
+        with MockPycurlInstaller(self, []):
+            with mock.patch(__name__ + ".tor_async_util._logger") as logger_patch:
+                self.assertFalse(tor_async_util.is_libcurl_compiled_with_async_dns_resolver())
+
+                self.assertEqual(
+                    logger_patch.error.call_args_list,
+                    [])
+
+                self.assertEqual(
+                    logger_patch.info.call_args_list,
+                    [])
+
+                expected_debug_message = (
+                    "Error trying to figure out if libcurl is complied with "
+                    "async DNS resolver - list index out of range"
+                )
+                self.assertEqual(
+                    logger_patch.debug.call_args_list,
+                    [mock.call(expected_debug_message)])
+
+    def test_happy_path(self):
+        with MockPycurlInstaller(self, [0, 0, 0, 0, 1 << 7]):
+            with mock.patch(__name__ + ".tor_async_util._logger") as logger_patch:
+                self.assertTrue(tor_async_util.is_libcurl_compiled_with_async_dns_resolver())
+
+                self.assertEqual(
+                    logger_patch.error.call_args_list,
+                    [])
+
+                self.assertEqual(
+                    logger_patch.info.call_args_list,
+                    [])
+
+                self.assertEqual(
+                    logger_patch.debug.call_args_list,
+                    [])
