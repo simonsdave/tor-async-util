56b968371c282e9d7a1cbec68be5d8cd7e886812
simonsdave <simonsdave@gmail.com>
2018-01-13 19:34:53 -0500

    so we can start using prep-for-release-python.sh

---
+++ .prep-for-release-release-branch-changes.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 1 ]; then
+    echo "usage: $(basename "$0") <release-branch>" >&2
+    exit 1
+fi
+
+RELEASE_BRANCH=${1:-}
+
+sed -i -e \
+    "s|?branch=master|?branch=$RELEASE_BRANCH|g" \
+    "$SCRIPT_DIR_NAME/README.md"
+
+sed -i -e \
+    "s|(docs|(https://github.com/simonsdave/tor-async-util/tree/$RELEASE_BRANCH/docs|g" \
+    "$SCRIPT_DIR_NAME/README.md"
+
+rm -f "$SCRIPT_DIR_NAME/README.rst"
+pandoc "$SCRIPT_DIR_NAME/README.md" -o "$SCRIPT_DIR_NAME/README.rst"
+
+rm -rf "$SCRIPT_DIR_NAME/dist"
+python "$SCRIPT_DIR_NAME/setup.py" bdist_wheel sdist --formats=gztar
+
+exit 0
