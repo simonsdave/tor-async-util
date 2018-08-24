0f9327325c00aae1c79184b513b87e95f03b8ca1
simonsdave <simonsdave@gmail.com>
2018-08-24 17:35:28 -0500

    start of new dev env approach

---
+++ tmp/tor-async-util-dev-env/build-docker-image.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This script ...
+#
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+TAG=latest
+IMAGE_NAME="simonsdave/tor-async-util-dev-env:$TAG"
+
+docker build -t "$IMAGE_NAME" "$SCRIPT_DIR_NAME/."
+
+exit 0
