c3b16ac8508db955b03ed0c40f8f0cdcb3845e95
simonsdave <simonsdave@gmail.com>
2018-08-27 22:22:22 -0500

    next iteration of docker based build process

--- tmp/tor-async-util-dev-env/build-docker-image.sh
+++ tmp/tor-async-util-dev-env/build-docker-image.sh
@@ -12,9 +12,23 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
+CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+PROJECT_HOME_DIR="$SCRIPT_DIR_NAME/../.."
+cp "$PROJECT_HOME_DIR/requirements.txt" "$CONTEXT_DIR/."
+cp "$PROJECT_HOME_DIR/setup.py" "$CONTEXT_DIR/."
+mkdir "$CONTEXT_DIR/tor_async_util"
+cp "$PROJECT_HOME_DIR/tor_async_util/__init__.py" "$CONTEXT_DIR/tor_async_util/."
+mkdir "$CONTEXT_DIR/bin"
+cp "$PROJECT_HOME_DIR/bin/tor_async_util_nosetests.py" "$CONTEXT_DIR/bin/."
+
 TAG=latest
 IMAGE_NAME="simonsdave/tor-async-util-dev-env:$TAG"
 
-docker build -t "$IMAGE_NAME" "$SCRIPT_DIR_NAME/."
+docker build \
+    -t "$IMAGE_NAME" \
+    --file "$SCRIPT_DIR_NAME/Dockerfile" \
+    "$CONTEXT_DIR"
+
+rm -rf "$CONTEXT_DIR"
 
 exit 0
