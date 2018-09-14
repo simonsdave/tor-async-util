2ff96aa9031cb8790f3b30e13061f5c16264924b
simonsdave <simonsdave@gmail.com>
2018-09-13 21:24:10 -0500

    more debug unit tests on travis

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
 if [ $# != 0 ]; then
     echo "usage: $(basename "$0")" >&2
     exit 1
@@ -11,6 +13,6 @@ fi
 USER=$(stat -c "%u" "$0")
 GROUP=$(stat -c "%g" "$0")
 
-docker run --rm --volume ~/tor-async-util:/app simonsdave/tor-async-util-dev-env:latest run_unit_tests.sh "$USER" "$GROUP"
+docker run --rm --volume "$SCRIPT_DIR_NAME/..:/app" simonsdave/tor-async-util-dev-env:latest run_unit_tests.sh "$USER" "$GROUP"
 
 exit 0
